eb54abac9f08e18223132281c7f8deeb10e25312
chrisidenbui <38187181+chrisidenbui@users.noreply.github.com>
2022-10-03 22:42:01 +0700

    axios and request

---
+++ src/services/SurveyServices.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+import { UPLOAD_SURVEY_URL } from "./../constant";
+
+let axiosConfig = {
+  headers: {
+    "Content-Type": "application/json;charset=UTF-8",
+    "Access-Control-Allow-Origin": "*",
+  },
+};
+
+const uploadSurvey = (data) => {
+  const body = {
+    sentence: data,
+  };
+  const post_request = axios.post(UPLOAD_SURVEY_URL, body, axiosConfig);
+  return post_request.then((response) => response.data);
+};
+
+export default uploadSurvey;
