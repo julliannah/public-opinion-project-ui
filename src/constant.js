1ea59f8b6cfc45ccc19031913163f1d9ce4a5c79
Long Tran <s3755614@rmit.edu.vn>
2022-10-16 23:12:31 +0700

    Test Survey API

--- src/constant.js
+++ src/constant.js
@@ -1,3 +1,3 @@
-const LOCALHOST = "http://34.214.238.252:8080/";
+const LOCALHOST = "https://surveyapi.publicsentimentapp.com:8080/";
 const CURRENT_HOST_URL = LOCALHOST;
 export const UPLOAD_SURVEY_URL = CURRENT_HOST_URL + "surveys/create";
